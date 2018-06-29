9d7d947932e44cb74a17cca59531a3a7e7f111df
messagecloudaroberts <adam@messagecloud.com>
2018-06-29 15:49:08 +0100

    first commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    {title: 'Javascript: The Good Parts'},
+    {title: 'Night World'},
+    {title: 'The Dark Tower'},
+    {title: 'Eloquent Ruby'}
+  ]
+}
